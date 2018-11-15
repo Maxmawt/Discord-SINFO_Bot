7f507e37ad409301f00707dc9680b19b7f066e79
Sami <sami.bosch@gmail.com>
2018-11-15 15:24:49 +0100

    Welcome message

--- Bot/run.py
+++ Bot/run.py
@@ -2,6 +2,7 @@
 
 # This file contains all functions necessary to start up the bot
 
+import discord
 from discord.ext.commands import Bot
 from message_parser import init
 
@@ -11,5 +12,18 @@ def runbot(token):
     client = Bot(command_prefix="!")
 
     init(client)
+
+    # All events unrelated to message parsing go here
+
+    @client.event
+    async def on_member_join(member):
+        server = member.server
+        welcome = discord.utils.get(server.channels, name="welcome")
+        client.send_message(welcome, "Bonjour {} et bienvenue dans le serveur SINFO! Indique ton nom et ta section "
+                                     "d'études (p.ex. 'Ingo Bot - sinf') ici pour qu'un modérateur puisse te vérifier "
+                                     "et donner accès au reste du serveur.\n\nHey there and welcome {}! Please give "
+                                     "your name and what studies you're following (e.g. 'Ingo Bot - sinf') so a "
+                                     "moderator can verify you and give you access to the rest of the server."
+                                     "".format(member.mention, member.mention))
 
     client.run(token)
