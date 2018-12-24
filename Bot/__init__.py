0bdc6b070bcc4f716de75951217eb006976845a3
Sami <sami.bosch@gmail.com>
2018-12-24 09:17:01 +0100

    Small fix to config

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -4,8 +4,13 @@
 
 import json
 from run import runbot
+import os
 
 
-with open('config.json') as f:
+config = '../config.json'
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, config)
+
+with open(filename) as f:
     data = json.load(f)
 runbot(data["Token"])
