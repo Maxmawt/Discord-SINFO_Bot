6cbdf13d4a9f1c456a548298b90cf600e43c505b
Sami <sami.bosch@gmail.com>
2018-12-25 18:13:36 +0100

    Functioning welcome message

--- Bot/run.py
+++ Bot/run.py
@@ -19,11 +19,12 @@ def runbot(token):
     async def on_member_join(member):
         server = member.server
         welcome = discord.utils.get(server.channels, name="welcome")
-        client.send_message(welcome, "Bonjour {} et bienvenue dans le serveur SINFO! Indique ton nom et ta section "
-                                     "d'études (p.ex. 'Ingi Bot - sinf') ici pour qu'un modérateur puisse te vérifier "
-                                     "et te donner accès au reste du serveur.\n\nHey there and welcome {}! Please give "
-                                     "your name and the studies you're following (e.g. 'Ingi Bot - sinf') so a "
-                                     "moderator can verify you and give you access to the rest of the server."
-                                     "".format(member.mention, member.mention))
+        await client.send_message(welcome,
+                                  "Bonjour %s et bienvenue dans le serveur SINFO! Indique ton nom et ta section "
+                                  "d'études (p.ex. 'Ingi Bot - sinf') ici pour qu'un modérateur puisse te vérifier "
+                                  "et te donner accès au reste du serveur.\n\nHey there and welcome %s! Please give "
+                                  "your name and the studies you're following (e.g. 'Ingi Bot - sinf') so a "
+                                  "moderator can verify you and give you access to the rest of the server."
+                                  % (member.mention, member.mention))
 
     client.run(token)
